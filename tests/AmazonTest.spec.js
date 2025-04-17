dbddc26a842edf04d3f7290cf14489bad77da3d3
Oshioke Adolphus <oshiokeadolphus@1a:b6:e8:3a:be:08.home>
2025-04-17 17:19:12 -0400

    Fifth Commit

--- tests/AmazonTest.spec.js
+++ tests/AmazonTest.spec.js
@@ -7,7 +7,7 @@ test.only('Validate a user can navigate to Amazon URL', async function({browser}
      const page = await newContent.newPage()
 
      await page.goto('https://www.amazon.in/')
-     await expect(page.locator('.nav-logo-link')).toHaveAttribute('aria-label', 'Amazon.in')
+     await expect(page.locator('.nav-logo-link')).toHaveAttribute('aria-label', 'Amazon.in',{timeout:10000})
 })
 test('Navigate to television section',async function({page}){
     const MainPage = new MainPage(page)
